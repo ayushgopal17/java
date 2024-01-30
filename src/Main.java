cb9b66c6e671c7e7a91faa97899427ca548f276a
ayushgopal <ayushgopal17@gmail.com>
2024-01-30 19:19:48 +0530

    name writing output code

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,13 @@
-//import java.lang.*;
-
-//public class Main {
-  //  public static void main(String[] args)
-    //{
-      //  System.out.println("Hello Wo
 import java.lang.*;
 import java.util.*;
+ class Main
+ {
+     public static void main(String[] args) {
+         String name;
+         Scanner sc=new Scanner(System.in);
 
-public class Main
-{
-        public static void main(String[] args)
-        {
-  Scanner sc=new Scanner(System.in)   ;
-
-                int x,y;
-                System.out.println("Enter 2 numbers");
-                x=sc.nextInt();
-                y=sc.nextInt();
-
-                int z=x+y;
-                System.out.println("sum is " +z );
-
-        }
-}
+         System.out.println("may i know your name");
+        name=sc.nextLine();
+         System.out.println("welcome " + name);
+     }
+ }
