5877e26c11f05225df1f16367dfc107aecc7b273
ayushgopal <ayushgopal17@gmail.com>
2024-01-31 19:28:44 +0530

    literals

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 import java.lang.*;
-import java.util.*;
- class Main
- {
-     public static void main(String[] args) {
-         String name;
-         Scanner sc=new Scanner(System.in);
 
-         System.out.println("may i know your name");
-        name=sc.nextLine();
-         System.out.println("welcome " + name);
-     }
- }
+class Main
+{
+public static void main(String[]args){
+        byte b=15;
+        short s=20;
+        int   i=30;
+    System.out.println(b);
+    System.out.println(s);
+    System.out.println(i);
+        }
+        }
