819d9c6022adf35807d10cbfc736fa9d6a991980
ayushgopal <ayushgopal17@gmail.com>
2024-02-03 15:07:55 +0530

    to find area of traingle with base and height

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+int B,H;
+        System.out.println("enter base");
+        B=sc.nextInt();
+        System.out.println("enter height");
+        H=sc.nextInt();
+        int ans=(B*H)/2;
 
 
-        System.out.println(10/(2*5));
+        System.out.println(ans);
     }
 
 }
