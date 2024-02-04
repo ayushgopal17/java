287e7fbbd8e7cb4893dbc9d473b788ba97bfbfc2
ayushgopal <ayushgopal17@gmail.com>
2024-02-04 19:29:03 +0530

    increment decrement with variables orderwise

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int x=5;
-        x++;
-        // we can use ++x here instead of x++ but when we have multiple variable and operations then this plays diffrent role
-        System.out.println(x);
+        int x=5,y=4,z;
+
+        z=2*x++ + 3 * ++y;
+
+        System.out.println(z);
 
     }
 
