951c2229bbcfea62f36b822696189434f66110f2
ayushgopal <ayushgopal17@gmail.com>
2024-02-04 19:22:28 +0530

    increment decrement with variables

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-
-float l,b,h,area,volume;
-        System.out.println("lenth of the cuboid");
-        l= sc.nextFloat();
-        System.out.println("breadth of the cuboid");
-        b= sc.nextFloat();
-        System.out.println("height of the cuboid");
-        h=sc.nextFloat();
-         area=2*((l*b)+(b*h)+(h*l));
-        System.out.println(" Total area of the cuboid is " + area);
-
-        volume=l*b*h;
-
-        System.out.println("volume of the cuboid is " + volume);
+        int x=5;
+        x++;
+        // we can use ++x here instead of x++ but when we have multiple variable and operations then this plays diffrent role
+        System.out.println(x);
 
     }
 
-}
+    }
+
