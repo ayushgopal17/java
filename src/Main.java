3e245f72577021eb9778a1c209ae2c7383cc9859
ayushgopal <ayushgopal17@gmail.com>
2024-02-07 15:19:42 +0530

    printing

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
- byte b=5;
- short s=120;
- int i=10;
- //flost f=10;
- float f=10.5f;
- double d=10;
- char c=10;
- boolean bl=true;
+
+
+          int a=10;
+          float b=10.55f;
+          char c='A';
+          String str="hello";
+
+        System.out.println(a);
         System.out.println(b);
-        i=s;
-     //   l=s;
-       // f=s;
-        d=s;
-
-        i=(int)f;
-        System.out.println(i);
-
-
+        System.out.println(c);
+        System.out.println(str);
 
     }
 
