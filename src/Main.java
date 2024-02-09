9ff9c3fe1ef27ea19b79fb0dc6078ea2274f6f15
ayushgopal <ayushgopal17@gmail.com>
2024-02-09 16:50:10 +0530

    string

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
-          int x=11;
-          float y=12.56f;
-          char z='A';
-          String str="java program";
-
-        System.out.printf("Hello %s \n",str);
-    }
+        String str1="java programm";
+        System.out.println(str1);
 
     }
+    }
+
 
