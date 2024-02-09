f96f07f1c47217aa7ab63d2f8788893d0c13b500
ayushgopal <ayushgopal17@gmail.com>
2024-02-09 17:24:46 +0530

    String pactice

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String str1="java programm";
-        String str2=new String("java");
+        String str1="java";
+        String str2="java";
 
-        char c[]={'h','e','l','l','o'};
-        String str3=new String (c,1,3);
-
-        byte b[]={65,66,67,68};
-
-        String str4=new String(b,1,2);
-
-
-        System.out.println(str3);
-
-
+        System.out.println(str1==str2);
 
     }
     }
