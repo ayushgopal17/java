eed3c47d34d919f1d1c293eadfdb82e4eb236b68
ayushgopal <ayushgopal17@gmail.com>
2024-02-09 17:20:16 +0530

    stringa

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,11 @@ public class Main {
         String str2=new String("java");
 
         char c[]={'h','e','l','l','o'};
-        String str3=new String (c);
+        String str3=new String (c,1,3);
 
         byte b[]={65,66,67,68};
 
-        String str4=new String(b);
+        String str4=new String(b,1,2);
 
 
         System.out.println(str4);
