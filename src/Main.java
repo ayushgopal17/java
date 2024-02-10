ee7cbe395d88a6d443a16d6a2398bb2f2f167246
ayushgopal <ayushgopal17@gmail.com>
2024-02-10 19:02:49 +0530

    string uppercase

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-     String str=new String("netbeans");
+   String str=new String("netbeans");
 
-        System.out.println(str.length());
+
+      String   str2=str.toUpperCase();
+        System.out.println(str2);
+
     }
     }
 
