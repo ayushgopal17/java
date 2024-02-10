632343520f89ee8d54916a06fd836f3657353e5b
ayushgopal <ayushgopal17@gmail.com>
2024-02-10 13:43:14 +0530

    String pactice and equality

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         String str1="java";
-        String str2="java";
+        String str2=new String("java");
 
         System.out.println(str1==str2);
 
