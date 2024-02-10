b608564773a8b55406bcad6be8241f12b5c7210c
ayushgopal <ayushgopal17@gmail.com>
2024-02-10 18:59:27 +0530

    string

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String str1="java";
-        String str2=new String("java");
+     String str=new String("netbeans");
 
-        System.out.println(str1==str2);
-
+        System.out.println(str.length());
     }
     }
 
