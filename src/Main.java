0505778154128c82fe139ac221a6a13cdb60c9cb
ayushgopal <ayushgopal17@gmail.com>
2024-02-11 20:31:28 +0530

    string practice; loop

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String str1="mr. Shahrukh khan";
+        String str1 = "mr. Shahrukh khan";
 
-        System.out.println(  str1.endsWith("khan"));
+        for (int i = 0; i < str1.length(); i++)
 
+            System.out.println(str1.charAt(i));
 
     }
-}
+    }
 
 
 
