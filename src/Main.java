f61d3deee9135c641d8edf480a675712a13a2470
ayushgopal <ayushgopal17@gmail.com>
2024-02-12 23:22:28 +0530

    string practice; contains

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String str1 = "china wall";
+        String str1 = "the great wall of china wall";
 
-        String str2 =new String ("pyramid");
+       // String str2 =new String ("china Wall");
 
-        System.out.println(str1.compareTo(str2));
+        System.out.println(str1.contains("wall"));
 
     }
 }
