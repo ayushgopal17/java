8f01fbac8c9ef8863c822ac3e070248ebb0177fe
ayushgopal <ayushgopal17@gmail.com>
2024-02-12 23:24:50 +0530

    string practice; string addition

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String str1 = "the great wall of china wall";
+        String str1 = "the great wall";
 
-       // String str2 =new String ("china Wall");
+       String str2=  " of china ";
 
-        System.out.println(str1.contains("wall"));
+        System.out.println(str1+ str2);
 
     }
 }
