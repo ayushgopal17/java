ceed86842ea0f3a0dc39b9f3a6dc760327bbf6c5
ayushgopal <ayushgopal17@gmail.com>
2024-02-12 23:15:17 +0530

    string practice; compare to

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String str1 = "pyramid";
+        String str1 = "Pyramid";
 
-        String str2 = "Pyramid";
+        String str2 =new String ("Pyramid");
 
-        System.out.println(str1.equalsIgnoreCase(str2));
+        System.out.println(str1.compareTo(str2));
 
     }
 }
-
