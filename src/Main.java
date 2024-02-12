614b5938b86c1d96997f366780be7542b818a119
ayushgopal <ayushgopal17@gmail.com>
2024-02-12 23:56:57 +0530

    string practice; mathes

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String str1 = "the great wall";
+        String str1 = "f";
 
        String str2=  " of china ";
 
-        System.out.println(str1+ str2);
+        System.out.println(str1.matches("."));
 
     }
 }
