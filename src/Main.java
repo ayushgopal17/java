9050d59496bae4783a079fc4f56169fbf860502e
ayushgopal <ayushgopal17@gmail.com>
2024-02-12 23:17:24 +0530

    string practice; compare to and  it gives diffrence

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String str1 = "pyramid";
+        String str1 = "china wall";
 
-        String str2 =new String ("Pyramid");
+        String str2 =new String ("pyramid");
 
         System.out.println(str1.compareTo(str2));
 
