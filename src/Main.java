d636203be9b4304a2397d2581c69bb2f804f6dba
ayushgopal <ayushgopal17@gmail.com>
2024-02-13 00:17:10 +0530

    string practice; matches

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String str1="7";
+        String str1="a7";
 
 
-        System.out.println(str1.matches("[a-z0-9]"));
+        System.out.println(str1.matches("[a-z][0-9]"));
 
     }
 }
