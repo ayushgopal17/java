7117bbd0075b71fbd490c4001abd37674857f26c
ayushgopal <ayushgopal17@gmail.com>
2024-02-13 00:05:15 +0530

    string practice; matches

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String str1 = "abc";
+        String str1="7";
 
 
-
-       // here it required three dots because there are three alphabets in the string 1
-        System.out.println(str1.matches("..."));
+        System.out.println(str1.matches("[a-z0-9]"));
 
     }
 }
