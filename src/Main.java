639ff0790df451a26906050122ad9d12bab13358
ayushgopal <ayushgopal17@gmail.com>
2024-02-14 21:52:15 +0530

    string practice; matches: *and [abc]

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String str1="abcdef";
+        String str1="abbbcccaaac";
 
-        System.out.println(str1.matches("[a-z]*"));
+        System.out.println(str1.matches("[abc]*"));
 
 
     }
