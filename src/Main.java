702acdd7117c519bcbf4b4fbb85b41063a73a3b3
ayushgopal <ayushgopal17@gmail.com>
2024-02-14 21:55:19 +0530

    string practice; matches: + and [abc]{3}

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String str1="";
+        String str1="acc";
 
         // this wiil be false beacuse + means 1 or more and * means 0 or more
 
-        System.out.println(str1.matches("[abc]+"));
+        System.out.println(str1.matches("[abc]{3}"));
 
 
     }
