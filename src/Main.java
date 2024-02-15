9dfbcd4b62c8a76425591cbd86bac104b6bd3b93
ayushgopal <ayushgopal17@gmail.com>
2024-02-15 20:00:59 +0530

    student challange 2

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-     String str1="programmer@gmail.com";
-
-       int i=str1.indexOf("@");
-
-       String uname= str1.substring(0,i);
-       String domainname=str1.substring(i+1, str1.length());
-
-        System.out.println("username = "+uname);
-        System.out.println("domainname = "+domainname);
-        System.out.println(domainname.startsWith("gmail.com"));
-
-      int j=domainname.indexOf(".");
-       String name=domainname.substring(0,j);
-        System.out.println(name.equals("gmail"));
-
+     int b=1100010;
+     String str=b+"";
+        System.out.println(str.matches("[01]*"));
 
     }
 }
