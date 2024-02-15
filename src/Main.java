b75e7c3fc5027d4eb6d9d2c8312d86be7e7e6f80
ayushgopal <ayushgopal17@gmail.com>
2024-02-15 19:33:39 +0530

    student challange

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
        String uname= str1.substring(0,i);
        String domainname=str1.substring(i+1, str1.length());
 
-        System.out.println(uname);
-        System.out.println(domainname);
+        System.out.println("username = "+uname);
+        System.out.println("domainname = "+domainname);
     }
 }
