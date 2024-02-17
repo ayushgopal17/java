8cd4485b6791cad2c0770a0e27d359f347d9cbae
ayushgopal <ayushgopal17@gmail.com>
2024-02-17 23:51:25 +0530

    conditionals for float

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ package Main;
 public class Main {
     public static void main(String[] args) {
 
-        int a=5, b=10, c=15;
+        float a=5.4f, b=10.5f, c=15;
 
-        System.out.println(a<b || a>c);
+        System.out.println(a<b );
     }
 }
