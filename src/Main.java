38cbd63cca1c4ff4591d966f35cad1e6f6137d9b
ayushgopal <ayushgopal17@gmail.com>
2024-02-18 01:45:40 +0530

    find grades of marks of a class

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,44 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-  Package main;
+        Package main;
 
-        Scanner scanner =new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("please enter a number");
-    int n = scanner.nextInt();
+        System.out.println("please enter  marks m1, m2, m3 ");
+        int m1 = scanner.nextInt();
+        int m2 = scanner.nextInt();
+        int m3 = scanner.nextInt();
 
-    if(n>=14 && n<=55  )
+
+        System.out.println("total =" + (m1 + m2 + m3));
+        int average =( (m1 + m2 + m3) / 3);
+
+        System.out.println(average);
+if (average>=70)
         {
-            System.out.println("you are young");
+            System.out.println("A");
         }
-        else {
-            System.out.println("you are not young");
+
+        else if (average>=60 && average<=70)
+        {
+            System.out.println("B");
         }
+
+else if (average>=50 && average<=60)
+{
+    System.out.println("C");
+}
+
+else if (average>=40 && average<=50)
+{
+    System.out.println("A");
+
+}
+else if (average<40)
+{
+    System.out.println("F");
+}
+
     }
 }
