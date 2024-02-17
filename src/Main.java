446072ad81a5a4a2d103261d23a84913bb6137f0
ayushgopal <ayushgopal17@gmail.com>
2024-02-18 01:21:01 +0530

    to check where you are young or not young

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+  Package main;
 
         Scanner scanner =new Scanner(System.in);
 
         System.out.println("please enter a number");
     int n = scanner.nextInt();
 
-    if(n%2<=0)
+    if(n>=14 && n<=55  )
         {
-            System.out.println("Even");
+            System.out.println("you are young");
         }
         else {
-            System.out.println("Odd");
+            System.out.println("you are not young");
         }
     }
 }
