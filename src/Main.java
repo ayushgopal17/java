029b5501519f6b77dac56f125e23c0e3ce8332f0
ayushgopal <ayushgopal17@gmail.com>
2024-02-21 01:09:20 +0530

    practicing switch case

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,21 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
-        System.out.println("Enter a URL ");
-        String URL=scan.nextLine();
+        int n = 3;
+        switch (n) {
 
-        String protocol =URL.substring(0, URL.indexOf(":"));
-
-
-        if(protocol.equals("http"))
-            System.out.println("Hyper text transfer protocol");
-
-         else if (protocol.equals ("ftp"))
-            System.out.println("file transfer protocol");
-
-        String ext=URL.substring(URL.lastIndexOf(".")+1);
-
-        if(ext.equals("com"))
-        System.out.println("commercial");
-            else if (ext.equals("org"))
-            System.out.println("organisation");
-        else if (ext.equals("net"))
-            System.out.println("network");
+            case 1:
+                System.out.println("one");
+                break ;
+            case 2 :
+                System.out.println("Two");
+                 break;
+            case 3 :
+                System.out.println("Three");
+                break;
+            default:  System.out.println("Invalid");
+        }
 
     }
 }
-
 
