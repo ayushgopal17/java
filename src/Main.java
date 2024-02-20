5972317c821757578d2f21666cdbf3fe56bb0cc0
ayushgopal <ayushgopal17@gmail.com>
2024-02-20 19:13:52 +0530

    entering day no. and knowing the day.

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,51 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
 
-        Package main;
-
-
-        Scanner scanner = new Scanner(System.in);
-
-        int day;
-
-        System.out.println("please enter a number number ");
-
-        day = scanner.nextInt();
-
-        if (day == 1) {
-            System.out.println("Monday");
-        } else if (day == 2) {
-            System.out.println("Tuesday");
-        } else if (day == 3) {
-            System.out.println("wednesday");
-        } else if (day == 4) {
-            System.out.println("Thusday");
-        } else if (day == 5) {
-            System.out.println("Friday");
-        } else if (day == 6) {
-            System.out.println("saturday");
-        }
-        else if (day == 7) {
-            System.out.println("sunday");
-        }
-        else
+        System.out.println("Enter a day number");
+        int day=scan.nextInt();
+ if(day==1)
+ {
+     System.out.println("mon");
+ }
+        if(day==1)
         {
-            System.out.println("invalid Day number");
+            System.out.println("mon");
+        }
+        else if(day==2)
+        {
+            System.out.println("tue");
+        }
+        else if(day==3)
+        {
+            System.out.println("wed");
+        }
+      else  if(day==4)
+        {
+            System.out.println("thus");
+        }
+        else if(day==5)
+        {
+            System.out.println("fri");
+        }
+        else if(day==6)
+        {
+            System.out.println("sat");
+        }
+        else if(day==7)
+        {
+            System.out.println("mon");
+        }
+    else
+        {
+            System.out.println("not a valid date");
         }
     }
-}
 
+
+}
 
 
