a2da30c48e456c82ebbc1a35689c4ad7e2a13607
ayushgopal <ayushgopal17@gmail.com>
2024-02-21 02:10:20 +0530

    to know the month name by number

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,59 @@
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) {
-        int n = 3;
-        switch (n) {
 
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter a number");
+        int num=scanner.nextInt();
+        switch (num)
+        {
             case 1:
-                System.out.println("one");
-                break ;
-            case 2 :
-                System.out.println("Two");
-                 break;
-            case 3 :
-                System.out.println("Three");
+                System.out.println("jan");
                 break;
-            default:  System.out.println("Invalid");
+            case 2:
+                System.out.println("feb");
+                break;
+            case 3:
+                System.out.println("march");
+                break;
+            case 4:
+                System.out.println("april");
+                break;
+            case 5:
+                System.out.println("may");
+                break;
+            case 6:
+                System.out.println("june");
+                break;
+            case 7:
+                System.out.println("july");
+                break;
+
+            case 8:
+                System.out.println("aug");
+                break;
+
+            case 9:
+                System.out.println("sept");
+                break;
+
+            case 10:
+                System.out.println("oct");
+                break;
+
+            case 11:
+                System.out.println("nov");
+                break;
+
+            case 12:
+                System.out.println("dec");
+                break;
+            default:
+                System.out.println("invalid number");
+
+
+
         }
 
     }
