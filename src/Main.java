5fd96d41176cdb037fb249489f6346146881fbac
ayushgopal <ayushgopal17@gmail.com>
2024-02-20 20:11:38 +0530

    entering url and knowing its type

--- src/Main.java
+++ src/Main.java
@@ -4,48 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
+        System.out.println("Enter a URL ");
+        String URL=scan.nextLine();
 
-        System.out.println("Enter a day number");
-        int day=scan.nextInt();
- if(day==1)
- {
-     System.out.println("mon");
- }
-        if(day==1)
-        {
-            System.out.println("mon");
-        }
-        else if(day==2)
-        {
-            System.out.println("tue");
-        }
-        else if(day==3)
-        {
-            System.out.println("wed");
-        }
-      else  if(day==4)
-        {
-            System.out.println("thus");
-        }
-        else if(day==5)
-        {
-            System.out.println("fri");
-        }
-        else if(day==6)
-        {
-            System.out.println("sat");
-        }
-        else if(day==7)
-        {
-            System.out.println("mon");
-        }
-    else
-        {
-            System.out.println("not a valid date");
-        }
+        String protocol =URL.substring(0, URL.indexOf(":"));
+
+
+        if(protocol.equals("http"))
+            System.out.println("Hyper text transfer protocol");
+
+         else if (protocol.equals ("ftp"))
+            System.out.println("file transfer protocol");
+
+        String ext=URL.substring(URL.lastIndexOf(".")+1);
+
+        if(ext.equals("com"))
+        System.out.println("commercial");
+            else if (ext.equals("org"))
+            System.out.println("organisation");
+        else if (ext.equals("net"))
+            System.out.println("network");
+
     }
-
-
 }
 
 
