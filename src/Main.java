109790d820fc3335dcc5c8c4aa646ca4988bf573
ayushgopal <ayushgopal17@gmail.com>
2024-02-22 00:20:45 +0530

    to know the website and knowing types

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,25 @@
 import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("enter a number");
-        int num=scanner.nextInt();
-        switch (num)
+
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter a website");
+        String Website=scan.nextLine();
+        String ext=Website.substring(Website.lastIndexOf(".")+1);
+        switch (ext)
         {
-            case 0:
-                System.out.println("zero");
-             break;
-            case 1:
-                System.out.println("one");
+            case "com":
+                System.out.println("commercial");
                 break;
-            case 2:
-                System.out.println("two");
+            case "org":
+                System.out.println("organisation");
                 break;
-            case 3:
-                System.out.println("three");
+            case "gov":
+                System.out.println("government");
                 break;
-            case 4:
-                System.out.println("four");
-                break;
-            case 5:
-                System.out.println("five");
-                break;
-            case 6:
-                System.out.println("six");
-                break;
-            case 7:
-                System.out.println("seven");
-                break;
-            case 8:
-                System.out.println("eight");
-                break;
-            case 9:
-                System.out.println("nine");
-                break;
-            default:
-                System.out.println("invalid number");
+            case "net":
+                System.out.println("network");
                 break;
         }
 
