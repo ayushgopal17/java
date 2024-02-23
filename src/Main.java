81447d01063fac035c9d8d27dc0d3069fd16e1dc
ayushgopal <ayushgopal17@gmail.com>
2024-02-23 22:34:50 +0530

    infinite loop of byte

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args)
     {
-        int i=1;
+        byte i=1;
    while (true)
          {
             System.out.println(i);
