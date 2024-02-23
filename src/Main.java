006b5ef087c6accfa9a2a0788ef0822a79ba8c26
ayushgopal <ayushgopal17@gmail.com>
2024-02-23 22:29:31 +0530

    practicing do while loop

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         int i = 1;
-        while (i < 100)
+
+        do
         {
             System.out.println(i);
-        i = i*2; }
+        i = i*2; } while (i<100);
     }
 }
