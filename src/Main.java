96bc32782ef61be9439d634587b85e7cb3742b41
ayushgopal <ayushgopal17@gmail.com>
2024-02-23 22:34:07 +0530

    infinite loop

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-
-        int i = 1;
-
-        do
-        {
+    public static void main(String[] args)
+    {
+        int i=1;
+   while (true)
+         {
             System.out.println(i);
-        i = i*2; } while (i<100);
+            i++;
+        }
     }
 }
