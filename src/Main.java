78bef0c6ce23a411a5834294c4ac63633c110534
ayushgopal <ayushgopal17@gmail.com>
2024-02-24 23:57:00 +0530

    for loop reversed ii

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args)
     {
 
-        for(int i=10;i>1;i--)
+         // here we can put the initiallization i.e int i=0 & j=1 part and i++ and j=j*2 part
+       // after and before respectively also
+        for(int i=0,j=1 ;i<=10;i++,j=j*2)
         {
-            System.out.println(i);
+            System.out.println(i+" "+j);
         }
     }
 }
