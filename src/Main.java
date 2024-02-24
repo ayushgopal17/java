10be5218c264c0cadf9bb4fa67b8f02157fc4cb3
ayushgopal <ayushgopal17@gmail.com>
2024-02-25 00:04:57 +0530

    table of 5

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,10 @@ public class Main {
     public static void main(String[] args)
     {
 
-         // here we can put the initiallization i.e int i=0 & j=1 part and i++ and j=j*2 part
-       // after and before respectively also
-        for(int i=0,j=1 ;i<=10;i++,j=j*2)
+
+        for(int i=1 ;i<=10;i++)
         {
-            System.out.println(i+" "+j);
+            System.out.println("5*"+i+"="+5*i);
         }
     }
 }
