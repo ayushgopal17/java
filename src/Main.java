e06c1031178e1729003f6c783d5df63a88bc8771
ayushgopal <ayushgopal17@gmail.com>
2024-02-25 00:14:52 +0530

    table of any value input

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args)
     {
-
-
+      //  int n;
+       Scanner scanner= new Scanner(System.in);
+       int n=scanner.nextInt();
+        System.out.println("enter a number");
         for(int i=1 ;i<=10;i++)
         {
-            System.out.println("5*"+i+"="+5*i);
+            System.out.println(n+"X"+i+"="+n*i);
         }
     }
 }
