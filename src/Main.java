7122b8cf5b792cb8058f5e1d5e1c530b2cc5f479
ayushgopal <ayushgopal17@gmail.com>
2024-02-25 00:33:36 +0530

    sum of first n number is

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] args)
     {
-      //  int n;
+
        Scanner scanner= new Scanner(System.in);
        int n=scanner.nextInt();
         System.out.println("enter a number");
-        for(int i=1 ;i<=10;i++)
+
+        int sum=0;
+        for(int i=1 ;i<=n;i++)
         {
-            System.out.println(n+"X"+i+"="+n*i);
+            sum=sum+i;
         }
+        System.out.println("sum of " +n+ " Number is "+sum);
     }
 }
