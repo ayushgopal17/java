ee341cff97fd6f0992e4bd1a0c2c6f504a817f62
ayushgopal <ayushgopal17@gmail.com>
2024-02-28 19:13:12 +0530

    TO CHECK WHETHER A NUMBER IS ARMSTONG OR NOT

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number");
-        int n=scanner.nextInt();
-        int count=0;
-        while(n>0)
-        {
-          n=n/10;
-          count ++;
+        int n = scanner.nextInt();
+        int m = n;
+        int sum = 0;
+        while (n > 0) {
+            int r = n % 10;
+            sum = sum + r * r * r;
+            n = n / 10;
         }
-        System.out.println("The number of digits in this number is "+count);
+        System.out.println(sum);
+        if (sum == m)
+
+            System.out.println("Armstrong number");
+        else System.out.println("Not an Armstong number");
     }
 }
