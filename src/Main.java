91d3e05568567806f545713e689a664145666b75
ayushgopal <ayushgopal17@gmail.com>
2024-03-01 23:25:02 +0530

    displaying  anumber in words

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,54 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number");
         int n = scanner.nextInt();
-		int m=n;
-      int  rev=0;
-while (n>0)
-{
- int  r=n%10;
-   rev=rev*10+r;
-   n=n/10;
-}
-		System.out.println(rev);
-if (rev==m)
-	System.out.println("the number is palidrome");
-else
-	System.out.println("its not a palidrome");
+        String str="";
 
+		while (n>0)
+        {
+            int r = n % 10;
+            n = n / 10;
+            str = str + r;
+        }
+        System.out.println(str);
+  char c;
+for (int i=str.length()-1;i>=0;i--)
+{
+    c=str.charAt(i);
+
+    switch (c)
+    {
+        case '0':
+            System.out.print("zero ");
+               break;
+        case '1':
+            System.out.print("one ");
+            break;
+        case '2':
+            System.out.print("two ");
+            break;
+        case '3':
+            System.out.print("three  ");
+            break;
+        case '4':
+            System.out.print("four ");
+            break;
+        case '5':
+            System.out.print("five ");
+            break;
+        case '6':
+            System.out.print("six ");
+            break;
+        case '7':
+            System.out.print("seven ");
+            break;
+        case '8':
+            System.out.print("eight ");
+            break;
+        case '9':
+            System.out.print("nine ");
+            break;
+
+    }
+}
     }
 }
