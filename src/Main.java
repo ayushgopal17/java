e9de90ed45330b31f18c4d02d2eb4c4552bf8db7
ayushgopal <ayushgopal17@gmail.com>
2024-03-02 19:45:48 +0530

    star pattern decresing starting from left

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@ public class Main
 {
     public static void main(String[] args)
     {
-// decresing star pattern
+
         for (int i=1;i<=5;i++)
         {
             for (int j=1;j<=5;j++)
         {
-           if(i<=j)
+           if(i+j>5)
                System.out.print("*");
            else
                System.out.print(" ");
