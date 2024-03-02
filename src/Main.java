24165bfdd73f8aec8914a7493c6b092c96b0b065
ayushgopal <ayushgopal17@gmail.com>
2024-03-02 14:04:07 +0530

    program for printing GP series

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Program to print AP series");
-        System.out.println("Enter a, d, and n");
-        int a = scanner.nextInt();
-        int d = scanner.nextInt();
-        int n = scanner.nextInt();
 
-     int term=a;
-     for(int i=0;i<n;i++)
-     {
-         System.out.print(term+" ");
-               term=term+d;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("printing GP of a number");
+        System.out.println("Enter a, r and n");
 
-     }
-}
+        int a =scanner.nextInt();
+        int r =scanner.nextInt();
+        int n =scanner.nextInt();
+
+         int term =a;
+        for (int i=0;i<n;i++)
+        {
+            System.out.print(term+" ");
+             term=term*r;
+        }
     }
-
+}
