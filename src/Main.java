ffeec5b3effadce7a62efc3d7bdbbb7a26595f36
ayushgopal <ayushgopal17@gmail.com>
2024-03-02 15:34:01 +0530

    patterns 1 ii

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 for (int i=1;i<=5;i++)
    {  for (int j = 1; j<=5;j++)
         {
-            System.out.print(j+" ");
+            System.out.print(i+" ");
         }
     System.out.println(" ");
         }
