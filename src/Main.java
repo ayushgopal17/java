fffabc01efca7072471d4070f0d300e72e37195b
ayushgopal <ayushgopal17@gmail.com>
2024-03-02 16:00:43 +0530

    star pattern

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,13 @@ public class Main
 {
     public static void main(String[] args)
     {
-       int count=0;
-        for (int i=1;i<=5;i++)
-        {
-            for(int j=1;j<=i;j++)
+        for (int i=1;i<=5;i++){
+            for (int j=1;j<=i;j++)
             {
-                count++;
-                System.out.print(count+" ");
+                System.out.print("* ");
             }
             System.out.println("");
         }
-        }
-    }
+         }
+           }
 
