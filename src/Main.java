18fd6637e11c5a5ccd37da7ca5cd17b3dbd887fd
ayushgopal <ayushgopal17@gmail.com>
2024-03-02 15:51:48 +0530

    1 2 3 pattern

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
+
 import java.util.*;
-public class Main {
-    public static void main(String[] args) {
-        int count=0;
-for (int i=1;i<=5;i++)
-   {  for (int j = 1; j<=5;j++)
+public class Main
+{
+    public static void main(String[] args)
+    {
+        for (int i=1;i<=5;i++)
         {
-            ++count;
-            System.out.format("%2d ",count);
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println("");
         }
-    System.out.println(" ");
         }
     }
-    }
+
