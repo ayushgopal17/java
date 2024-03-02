167acfcc9ca71aa7593c56d3ac9d7cc76d89a816
ayushgopal <ayushgopal17@gmail.com>
2024-03-02 14:26:32 +0530

    program for printing fibonnaci series

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,22 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner=new Scanner(System.in);
-        System.out.println("printing GP of a number");
-        System.out.println("Enter a, r and n");
+        System.out.println("printing to print fibonacci series");
+        System.out.println("Enter a the Terms");
 
-        int a =scanner.nextInt();
-        int r =scanner.nextInt();
         int n =scanner.nextInt();
 
-         int term =a;
-        for (int i=0;i<n;i++)
+        int a=0,b=1,c;
+        System.out.print(a+"," +b+",");
+
+        for (int i=0;i<n-2;i++)
         {
-            System.out.print(term+" ");
-             term=term*r;
+            c=a+b;
+            System.out.print(c+",");
+            a=b;
+            b=c;
+
         }
-    }
+        }
+
 }
