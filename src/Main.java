5d349fd8618948692762def3b43183242efcfbbe
ayushgopal <ayushgopal17@gmail.com>
2024-03-02 19:07:03 +0530

    decresing star pattern withzeroes

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,13 @@ public class Main
 {
     public static void main(String[] args)
     {
-        // decresing start pattern
+        int count=0;
         for (int i=1;i<=5;i++)
-        {for (int j=1;j<=5-i+1;j++)
         {
-            System.out.print("* ");
+            for (int j=1;j<=5-i+1;j++)
+        {
+            count++;
+            System.out.format("%02d ",count);
         }
             System.out.println("");
         }
