33bdd49ef0b24b04e4cd0704842ec543a064af63
ayushgopal <ayushgopal17@gmail.com>
2024-03-02 15:55:07 +0530

    count pattern

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,13 @@ public class Main
 {
     public static void main(String[] args)
     {
+       int count=0;
         for (int i=1;i<=5;i++)
         {
             for(int j=1;j<=i;j++)
             {
-                System.out.print(j+" ");
+                count++;
+                System.out.print(count+" ");
             }
             System.out.println("");
         }
