74656a070c817c6ac9471ae530099fdf97de4f3b
ayushgopal <ayushgopal17@gmail.com>
2024-03-02 13:51:23 +0530

    program for printing AP series

--- src/Main.java
+++ src/Main.java
@@ -2,56 +2,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number");
+        System.out.println("Program to print AP series");
+        System.out.println("Enter a, d, and n");
+        int a = scanner.nextInt();
+        int d = scanner.nextInt();
         int n = scanner.nextInt();
-        String str="";
 
-		while (n>0)
-        {
-            int r = n % 10;
-            n = n / 10;
-            str = str + r;
-        }
-        System.out.println(str);
-  char c;
-for (int i=str.length()-1;i>=0;i--)
-{
-    c=str.charAt(i);
+     int term=a;
+     for(int i=0;i<n;i++)
+     {
+         System.out.print(term+" ");
+               term=term+d;
 
-    switch (c)
-    {
-        case '0':
-            System.out.print("zero ");
-               break;
-        case '1':
-            System.out.print("one ");
-            break;
-        case '2':
-            System.out.print("two ");
-            break;
-        case '3':
-            System.out.print("three  ");
-            break;
-        case '4':
-            System.out.print("four ");
-            break;
-        case '5':
-            System.out.print("five ");
-            break;
-        case '6':
-            System.out.print("six ");
-            break;
-        case '7':
-            System.out.print("seven ");
-            break;
-        case '8':
-            System.out.print("eight ");
-            break;
-        case '9':
-            System.out.print("nine ");
-            break;
-
-    }
+     }
 }
     }
-}
+
