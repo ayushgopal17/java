ed8b6c322302f95527de3339cdd6c04d44da0fdc
ayushgopal <ayushgopal17@gmail.com>
2024-03-02 16:09:10 +0530

    decresing pattern ii

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main
         for (int i=1;i<=5;i++)
         {for (int j=1;j<=5-i+1;j++)
         {
-            System.out.print(j+" ");
+            System.out.print(i+" ");
         }
             System.out.println("");
         }
