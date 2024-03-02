39650049627b7ca8b9af1cde77b1f51393eec75b
ayushgopal <ayushgopal17@gmail.com>
2024-03-02 15:43:42 +0530

    number pattern from 1 to 25 ii

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ for (int i=1;i<=5;i++)
    {  for (int j = 1; j<=5;j++)
         {
             ++count;
-            System.out.print(count+" ");
+            System.out.format("%2d ",count);
         }
     System.out.println(" ");
         }
