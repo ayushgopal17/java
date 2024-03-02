7c63abc8b90c81ae858d1fa8fae98613e20db92a
ayushgopal <ayushgopal17@gmail.com>
2024-03-02 19:41:24 +0530

    star pattern decresing

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,15 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int count=0;
+
         for (int i=1;i<=5;i++)
         {
-            for (int j=1;j<=5-i+1;j++)
+            for (int j=1;j<=5;j++)
         {
-            count++;
-            System.out.format("%02d ",count);
+           if(i<=j)
+               System.out.print("*");
+           else
+               System.out.print(" ");
         }
             System.out.println("");
         }
