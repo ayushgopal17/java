3add6e3462791ba9e66c379c70493117eb1b6150
ayushgopal <ayushgopal17@gmail.com>
2024-03-02 14:58:46 +0530

    nested loop practice

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("printing to print fibonacci series");
-        System.out.println("Enter a the Terms");
-
-        int n =scanner.nextInt();
-
-        int a=0,b=1,c;
-        System.out.print(a+"," +b+",");
-
-        for (int i=0;i<n-2;i++)
+        for (int i = 1; i <= 5; i++)
         {
-            c=a+b;
-            System.out.print(c+",");
-            a=b;
-            b=c;
 
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("("+i+","+j+") ");
+            }
+            System.out.println("");
         }
-        }
-
+    }
 }
