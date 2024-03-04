9dd01658c585542b85105965f77c543940ff1538
ayushgopal <ayushgopal17@gmail.com>
2024-03-05 00:53:52 +0530

    sum of strings

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,16 @@ public class Main
 {
     public static void main(String[] args)
     {
-      int A[]=new int[10];
 
-      int B[]={1,2,3,4,5};
+        int A[] = {3,9,7,8,12,6,15,5,4,10};
 
-        System.out.println(B.length);
+     int  sum =0;
+
+     for(int i=0;i<A.length;i++)
+     {
+         sum=sum+A[i];
+     }
+        System.out.println("sum is "+sum);
     }
          }
 
