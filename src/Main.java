ea4758de1709e112db9b3f674be727193d6a2cd1
ayushgopal <ayushgopal17@gmail.com>
2024-03-05 01:12:30 +0530

    searching an element

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,25 @@ public class Main
 {
     public static void main(String[] args)
     {
-
+        Scanner scanner=new Scanner(System.in);
         int A[] = {3,9,7,8,12,6,15,5,4,10};
 
-     int  sum =0;
+        System.out.println("Enter a key");
 
-     for(int i=0;i<A.length;i++)
-     {
-         sum=sum+A[i];
+
+
+       int  key=scanner.nextInt();
+        for(int i=0;i<A.length;i++)
+        {
+        if(A[i]==key)
+        {
+            System.out.println("Element found at "+i);
+            System.exit(0);
+        }
+
+
      }
-        System.out.println("sum is "+sum);
+        System.out.println("Element not found");
     }
          }
 
