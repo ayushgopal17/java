d9df52191e463231be71ed837fdcb9178afdffce
ayushgopal <ayushgopal17@gmail.com>
2024-03-13 19:49:53 +0530

    copying an Array to B

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,18 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-
-        int A[]={0,1,2,3,4,5,6,7,8,9};
-        System.out.println("Lenth of A "+A.length);
+        int A[]={8,6,10,9,2,15,7,13,14,11};
         int B[]=new int[10];
 
         for (int i=0;i<A.length;i++)
         {
-
-            B[i]=A[i];
+            B[i] = A[i];
         }
-        A=B;
-        System.out.println("Length of A "+A.length);
-        }
+        for (int x:B)
+        System.out.print(x+",");
 
-}
+        }
+    }
 
 
 
