a1bdf50fddca0662298767666ac4557cc850ed86
ayushgopal <ayushgopal17@gmail.com>
2024-03-15 23:45:03 +0530

    prinyting zeroes

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,17 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        int A[][] = new int[5][5];
+        int A[] [];
+        A=new int[3][];
 
-        int B[][] = {{1, 2, 3}, {2, 4, 6}, {1, 3, 5}};
-
-        for (int x[] : B) {
-            for (int y : x) {
-                System.out.print(y);
+        A[0]=new int[5];
+         A[1]=new int[3];
+         A[2]=new int[8];
+         for (int i=0;i< A.length;i++)
+         {
+             for (int j=0;j<A[i].length;j++)
+             {
+                 System.out.print(A[i][j]+" ");
             }
             System.out.println("");
         }
