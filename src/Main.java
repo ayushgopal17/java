7376a00d6f8abec043fa7f7b2c8e28bbfb845d6b
ayushgopal <ayushgopal17@gmail.com>
2024-03-20 20:16:30 +0530

    multiplication of two matrices

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,32 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         int A[][] = {{3, 5, 9}, {7, 6, 2}, {4, 3, 5}};
-        int B[][] = {{1, 5, 2}, {6, 8, 4}, {3, 9, 7}};
+        int B[][] = {{1,0,0}, {0, 1, 0}, {0,0,1}};
 
-        int C[][] = new int[3][3];
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < A[0].length; j++)
+         int C[][]=new int [3][3];
+         for(int i=0;i<3;i++) {
+             for (int j = 0; j < 3; j++) {
+                 C[i][j] = 0;
+                 for (int K = 0; K < 3; K++) {
+                     C[i][j] = C[i][j] + A[i][K] * B[K][j];
+                 }
 
-            {  C[i][j]=A[i][j]+B[i][j];
-            }
-        }
-for(int x[]:C)
-{
-for(int y:x)
-
-    System.out.print(y+" ");
+             }
+         }
+     for(int x[]:C)
+     {
+         for(int y:x)
+         {
+             System.out.print(y+" ");
+         }
+         System.out.println("");
+     }
 }
-    System.out.println("");
+
+
 }
 
-    }
-}
+
 
 
 
