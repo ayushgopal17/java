345702958e5826aa760e141c0b153d445ac7f1ee
ayushgopal <ayushgopal17@gmail.com>
2024-03-20 19:17:34 +0530

    printing zeroes via for each loops

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,11 @@ public class Main {
         A[0]=new int[5];
          A[1]=new int[3];
          A[2]=new int[8];
-         for (int i=0;i< A.length;i++)
+         for (int x[]:A)
          {
-             for (int j=0;j<A[i].length;j++)
+             for (int y:x)
              {
-                 System.out.print(A[i][j]+" ");
+                 System.out.print(y+" ");
             }
             System.out.println("");
         }
