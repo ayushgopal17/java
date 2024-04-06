d57ce9285c640dc5fe8b8b49d688e5a3fe7a5e0c
ayushgopal <ayushgopal17@gmail.com>
2024-04-06 11:54:58 +0530

    max of two values using float

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,17 @@ public class Main {
     static int max(int x, int y) {
         return x > y ? x : y;
     }
-
+static float max(float x, float y)
+{
+    if(x>y)
+        return x;
+    else
+        return y;
+}
     public static void main(String[] args) {
 
 
-        System.out.println(max(10,5));
+        System.out.println(max(10.5f,5.6f));
     }
 }
 
