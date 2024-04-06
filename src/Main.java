2dbafad7bf829d8cdf409344acefc7fadcff2280
ayushgopal <ayushgopal17@gmail.com>
2024-04-06 11:22:56 +0530

    to find the gcd or hcf of two numbers

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,18 @@
 
 public class Main {
-    static boolean isPrime(int n)
-    {
-        for(int i=2;i<n/2;i++)
-        {
-            if(n%i==0)
-                return false;
+    static int gcd(int m, int n) {
+        while (m != n) {
+            if (m > n) m = m - n;
+            else n = n - m;
         }
-      return true;
-
-
+return m;
     }
 
     public static void main(String[] args) {
-        System.out.println(isPrime(97));
+        System.out.println(gcd(35,56));
     }
-
 }
+
 
 
 
