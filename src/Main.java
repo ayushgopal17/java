13613306f2005e10d208cc774544b3a468c4744f
ayushgopal <ayushgopal17@gmail.com>
2024-04-06 11:34:25 +0530

    max of two values

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
 
 public class Main {
-    static int gcd(int m, int n) {
-        while (m != n) {
-            if (m > n) m = m - n;
-            else n = n - m;
-        }
-return m;
+    static int max(int x, int y) {
+        return x > y ? x : y;
     }
 
     public static void main(String[] args) {
-        System.out.println(gcd(35,56));
+
+
+        System.out.println(max(10,5));
     }
 }
 
