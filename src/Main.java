63cdbaac1f064543ef008661a348152c8bf3eca9
ayushgopal <ayushgopal17@gmail.com>
2024-04-06 17:58:51 +0530

    variable arguments with showlist starting with 5

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 // here polymophism is used it means out of two methods whichever is required is being used out of these two
 public class Main {
-    static void showList(String ...S)
+    static void showList(int start,String ...S)
     { for(int i=0;i<S.length;i++) {
-        System.out.println(i + 1 + ". " + S[i]);
+        System.out.println(start+"."+S[i]);
+        start++;
     }
     }
 
     public static void main(String[] args) {
-        showList("Ajay","john","smith","ahmed","mark");
+        showList(5,"Ajay","john","smith","ahmed","mark");
     }
 }
 
