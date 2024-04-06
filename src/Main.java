f948b95ec0436e63f7a519a3761a8161d6babae9
ayushgopal <ayushgopal17@gmail.com>
2024-04-06 17:47:09 +0530

    variable arguments

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
 // here polymophism is used it means out of two methods whichever is required is being used out of these two
 public class Main {
-    static int max(int x, int y) {
-        return x > y ? x : y;
+    static void show(int ...A)
+    { for(int x:A)
+    {
+        System.out.println(x);
     }
-static int max(int x, int y,int z)
-{
-    if(x>y && x>z)
-        return x;
-    else if(y>z)
-        return y;
-    else
-        return z;
-}
+
+    }
+
     public static void main(String[] args) {
-        System.out.println(max(10,5,15));
-
-
+        show();
+        show(10,20,30);
+        show(new int[]{3,5,7,9,11,13,15});
     }
+
 }
 
 
