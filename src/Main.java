966889c3a1a81fd7edbba162caab32bf4bd45a97
ayushgopal <ayushgopal17@gmail.com>
2024-04-07 00:37:47 +0530

    finding the maximum values using varags

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
 // here polymophism is used it means out of two methods whichever is required is being used out of these two
 public class Main {
-    static void showList(int start,String ...S)
-    { for(int i=0;i<S.length;i++) {
-        System.out.println(start+"."+S[i]);
-        start++;
-    }
+    static int max(int ...A)
+    {
+        if(A.length==0) return Integer.MIN_VALUE;
+        int max=A[0];
+        for(int i=1;i<A.length;i++)
+            if(A[i]>max)max=A[i];
+        return max;
+
     }
 
     public static void main(String[] args) {
-        showList(5,"Ajay","john","smith","ahmed","mark");
+        System.out.println(max());
+        System.out.println(max(10));
+        System.out.println(max(10,20));
+        System.out.println(max(10,20,30));
     }
 }
+
 
 
 
