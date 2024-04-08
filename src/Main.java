8915714e29c8a2f8650a0263ff27477a4cb12db8
ayushgopal <ayushgopal17@gmail.com>
2024-04-08 14:55:55 +0530

    recursion

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,18 @@
-// here polymophism is used it means out of two methods whichever is required is being used out of these two
+
 public class Main {
-    static int max(int ...A)
+   static void fun(int n)
+   {
+       if(n>0)
+       {
+           System.out.println(n);
+           fun(n-1);
+       }
+   }
+
+    public static void main(String[] args)
     {
-        if(A.length==0) return Integer.MIN_VALUE;
-        int max=A[0];
-        for(int i=1;i<A.length;i++)
-            if(A[i]>max)max=A[i];
-        return max;
+        fun(3);
 
-    }
-
-    public static void main(String[] args) {
-        System.out.println(max());
-        System.out.println(max(10));
-        System.out.println(max(10,20));
-        System.out.println(max(10,20,30));
     }
 }
 
