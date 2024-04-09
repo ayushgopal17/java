d0983e27dcfa45aa86864db03c6b236913a53fde
ayushgopal <ayushgopal17@gmail.com>
2024-04-09 13:03:41 +0530

    object oriented programming

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,38 @@
+// package java1;
+class Main {
+    public double radius;
 
-public class Main {
-   static void fun(int n)
-   {
-       if(n>0)
-       {
-           System.out.println(n);
-           fun(n-1);
-       }
-   }
-
-    public static void main(String[] args)
+    public double area()
     {
-        fun(3);
+        return Math.PI * radius * radius;
+    }
 
+    public double perimeter()
+    {
+        return 2*Math.PI*radius;
+    }
+
+    public double circumference()
+    {
+        return perimeter();
     }
 }
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        Main c1 = new Main();
+        c1.radius = 7;
+
+        System.out.println(c1.area());
+        System.out.println(c1.perimeter());
+        System.out.println(c1.circumference());
+    }
+}
+
+
+
+
 
 
 
