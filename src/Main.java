56322a3acc744640e65143ccfc3ccb5a93ee067d
ayushgopal <ayushgopal17@gmail.com>
2024-04-11 01:24:27 +0530

    class for a students using opps

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,49 @@
-class cylinder
-{
+class students {
 
-    public double radius;
-    public double height;
+    public int roll;
+    public String name;
+    public String Course;
+    public int m1, m2, m3;
 
-
-    public double lidarea()
-    {
-        return  Math.PI * radius * radius;
+    public int total() {
+        return m1 + m2 + m3;
     }
 
-    public double totalsurfacearea()
-    { return 2*Math.PI*radius*(radius+height);
+    public float Average() {
+        return (float) total() / 3;
     }
-    public double volume(){
-        return Math.PI*radius*radius*height;
+
+    public char grade() {
+        if (Average() >= 60)
+            return 'A';
+        else
+            return 'B';
+
+    }
+
+
+    public String details() {
+        return "Roll no:" + roll + "\n" + "Name:" + name + "\n" + "Course:" +Course + "\n";
     }
 }
 
-public class Main
-{
+public class Main {
+
     public static void main(String[] args) {
-     cylinder c=new cylinder();
-     c.radius=7;
-     c.height=10;
-        System.out.println("lidarea is "+c.lidarea());
-        System.out.println("toTAL Surface area is "+c.totalsurfacearea());
-        System.out.println("volume is "+c.volume());
 
+        students s=new students();
+        s.roll=1;
+        s.name="Ayush" ;
+        s.Course="CS";
+        s.m1=70;
+        s.m2=80;
+        s.m3=65;
 
-
+        System.out.println("Total:"+s.total());
+        System.out.println("Average:"+s.Average());
+        System.out.println("Details:\n "+s.details());
     }
 }
-
 
 
 
