c1b075b0ed79052298152ae18498629b33cd09f9
ayushgopal <ayushgopal17@gmail.com>
2024-04-19 19:26:10 +0530

    parameter of constructor and two param

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 
 class parent
 {
+
      parent()
      {
          System.out.println("Non param of parent");
      }
      parent(int x)
      {
-         System.out.println("Param of parent ");
+         System.out.println("Param of parent "+x);
      }
     }
     class child extends parent
@@ -20,11 +21,17 @@ class parent
         {
             System.out.println("param of child");
         }
+        child(int x,int  y)
+        {
+            super(x);
+            System.out.println("2 param of child "+y);
+        }
+
     }
     public class Main
     {
         public static void main(String[] args) {
-            child c=new child();
+            child c=new child(10,20);
         }
 
     }
