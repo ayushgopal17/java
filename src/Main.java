4075d17e9007caad356fa474a8cbb9cc5bdf44e8
ayushgopal <ayushgopal17@gmail.com>
2024-04-22 18:52:46 +0530

    method overriding iii part 2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ class Car {
     public void accelerate(){System.out.println("Car is accelerated");}
     public void cargear(){System.out.println("car gear changes");}
    }
-   class luxurycar {
+   class luxurycar extends Car {
     public void Changegear(){
         System.out.println("Automatic gear changes");}
        public void openroof(){System.out.println("Sun roof is opened");
@@ -15,7 +15,7 @@ class Car {
         public class Main {
             public static void main(String[] args){
 
-                Car c=new Car();
+                luxurycar c=new luxurycar();
                 c.start();
                 c.accelerate();
                 c.cargear();
