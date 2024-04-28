b1ee10662df88a67a1224c07eea53b455e7fa52e
Ayush gopal <ayushgopal17@gmail.com>
2024-04-28 16:27:07 +0530

    final practice

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,10 @@
-class Test {
-    static {
-        System.out.println("Block 1");
-    }
-    static {
-        System.out.println("Block 2");
-    }
-}
 public class Main {
-    public static void main(String[] args)
-    {
-    Test t=new Test();
-    System.out.println("Main");
+
+    public static void main(String[] args) {
+        final float PI;
+        PI=3.14f;
+        System.out.println(PI);
+    }
 
     }
-}
+
