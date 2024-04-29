8ef8c11da56504ed6bc6685881dc86c8a2fbd56b
Ayush gopal <ayushgopal17@gmail.com>
2024-04-29 18:50:28 +0530

    student challenge on roll no using static members

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,34 @@
-public class Main {
+import java.util.*;
 
-    public static void main(String[] args) {
-        final float PI;
-        PI=3.14f;
-        System.out.println(PI);
-    }
+ class students {
+private String rollno;
+private static int count=1;
+private String asssigRollNo()
+{
+    Date d=new Date();
+    String rno="Univ-"+(d.getYear()+1900)+"-"+count;
+    count++;
+    return rno;
 
+}
+students()
+{
+    rollno=asssigRollNo();
+
+}
+public String getRollno()
+{
+    return rollno;
+}
     }
+    public class Main {
+        public static void main(String[] args) {
+            students s1=new students();
+            students s2=new students();
+            students s3=new students();
+            System.out.println(s1.getRollno());
+            System.out.println(s2.getRollno());
+            System.out.println(s3.getRollno());
+        }
+}
 
