691e7180e85d42ae0fbe842c8ebc7e61fbc77bb5
Ayush gopal <ayushgopal17@gmail.com>
2024-04-30 17:03:00 +0530

    checked unchecked exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,28 @@
-import java.util.Scanner;
+
+import java.io.*;
 
 public class Main {
+        static void fun1 ()
+        {
+
+            try {
+                System.out.println(10 / 0);
+            }
+                catch(Exception e)
+                {
+                    System.out.println(e.getMessage());
+            }
+        }
+        static void fun2()
+        {
+            fun1();
+        }
+        static void fun3()
+        {
+            fun2();
+        }
+
     public static void main(String[] args) {
-
-            int A[] = {30, 20, 10, 40, 0};
-          try {
-              int c = A[0] / A[2];
-
-              System.out.println("division is " + c);
-              try {
-                  System.out.println(A[5]);
-              }
-        catch(ArrayIndexOutOfBoundsException e)
-
-                  {
-                      System.out.println("Index is invalid");
-                  }
-
-              } catch (ArrayIndexOutOfBoundsException e) {
-                  System.out.println("Index is invalid");
-              }
-
-              System.out.println("bye");
-
-          }
+        fun3();
     }
-
-
+    }
