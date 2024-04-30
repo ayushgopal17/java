0f54d593f29d321251fda7630ce3cee84c4909d5
Ayush gopal <ayushgopal17@gmail.com>
2024-04-30 15:29:00 +0530

    Exception handling 0 in denominator

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+
 public class Main {
     public static void main(String[] args) {
-        int a=10,b=0,c;
-        c=a/b;
-        System.out.println(c);
+        int a = 10, b = 0, c;
+        try {
+
+
+            c = a / b;
+            System.out.println(c);
+        } catch (ArithmeticException e) {
+            System.out.println("denominator should not be 0, try again");
+        }
         System.out.println("bye");
     }
-}
+    }
+
 
