aed6310029f535a4316945290450eea7b3afe3fe
Ayush gopal <ayushgopal17@gmail.com>
2024-04-30 15:22:42 +0530

    Exception handling demo

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,9 @@
-import java.util.*;
-
- class students {
-private String rollno;
-private static int count=1;
-private String asssigRollNo()
-{
-    Date d=new Date();
-    String rno="Univ-"+(d.getYear()+1900)+"-"+count;
-    count++;
-    return rno;
-
-}
-students()
-{
-    rollno=asssigRollNo();
-
-}
-public String getRollno()
-{
-    return rollno;
-}
+public class Main {
+    public static void main(String[] args) {
+        int a=10,b=0,c;
+        c=a/b;
+        System.out.println(c);
+        System.out.println("bye");
     }
-    public class Main {
-        public static void main(String[] args) {
-            students s1=new students();
-            students s2=new students();
-            students s3=new students();
-            System.out.println(s1.getRollno());
-            System.out.println(s2.getRollno());
-            System.out.println(s3.getRollno());
-        }
 }
 
