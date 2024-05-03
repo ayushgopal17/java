fe160a8bf6a0691c43a0b5c7afa23e5af3314e9e
Ayush gopal <ayushgopal17@gmail.com>
2024-05-03 19:26:39 +0530

    synchronised keyword

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,49 @@
 
 
-class MyThread extends Thread
+class mydata
 {
+
+    public void display(String str)
+    {  synchronized(this) {
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i));
+        }
+    }
+    }
+}
+
+class MyThread1 extends Thread
+{
+    mydata d;
+    public MyThread1(mydata d)
+    {
+this.d=d;
+    }
     public void run()
     {
-        int count=1;
-        while (true) {
-
-            System.out.println(count++ +"My Thread");
-        }
+        d.display("hello world");
+    }
+}
+class MyThread2 extends Thread
+{
+    mydata d;
+    public MyThread2(mydata d)
+    {
+        this.d=d;
+    }
+    public void run()
+    {
+        d.display("Welcome All");
     }
 }
 public class Main {
-    public static void main(String[] args) throws Exception
-    {
-        MyThread t=new MyThread();
-        t.start();
-        int count=1;
+    public static void main(String[] args) throws Exception {
+mydata data=new mydata();
 
-        while (true)
-        {
-           System.out.println(count++ +"Main");
-           Thread.yield();
-        }
+MyThread1 t1=new MyThread1(data);
+MyThread2 t2=new MyThread2(data);
 
-        }
+   t1.start();
+   t2.start();
     }
+}
