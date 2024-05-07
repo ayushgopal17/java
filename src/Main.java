a94567498343c11e9d5f11229ed5f522ae837cb2
Ayush gopal <ayushgopal17@gmail.com>
2024-05-07 16:16:14 +0530

    user define annotation

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
-public class Main {
+import java.lang.annotation.Annotation;
+@interface myAnno
+{
+    String name();
+    String Project();
+    String Date() default "today";
+    String Version()default "13";
+}
+
+@myAnno (name ="Ajay",Project ="Bank")
+        public class Main
+{
+int data;
 
     public static void main(String[] args) {
-        String s1= new String("Hello");
-        StringBuffer s2 = new StringBuffer("Hello");
-        StringBuilder s3= new StringBuilder("Hello");
-
-        s1.concat("World");
-        s2.append("world");
-        s3.append("World");
-        System.out.println(s1);
-
-        System.out.println(s2);
-
-        System.out.println(s3);
+        int x;
     }
 }
