202f3acc13c883bc0124924f7bd0a7d0d67de9a9
Ayush gopal <ayushgopal17@gmail.com>
2024-05-08 12:17:48 +0530

    file input stream

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
-interface MyLambda
-{
-    public void display(String str);
-    }
-    public class Main {
-      public Main(String s) {
-          System.out.println(s.toUpperCase());
-      }
 
-        public static void main(String[] args) {
-            MyLambda ml=Main::new;
-            ml.display("hello");
+
+import java.io.*;
+
+public class Main{
+
+    public static void main(String[] args) throws Exception
+    {
+
+        try(FileInputStream fis=new FileInputStream("C:/MyJava/Test.txt");) {
+
+           int x;
+           do {
+               x = fis.read();
+
+               if (x != -1)
+                   System.out.println( (char)x);}
+           while (x!=-1);
+            {
+                System.out.println((char)x);
+            }
         }
-      }
+    }
 
+}
 
 
