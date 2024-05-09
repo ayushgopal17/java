4bbdfb51669e4517b9d98dad51181e89889fd470
Ayush gopal <ayushgopal17@gmail.com>
2024-05-09 19:59:31 +0530

    piped streams

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,81 @@
-
-
-
 import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        byte b[]={'a','b','c','d','e','f','g'};
-        ByteArrayInputStream bis=new ByteArrayInputStream(b);
-        String str=new String(bis.readAllBytes());
-        System.out.println(str);
-        bis.close();
+class Producer extends Thread
+{
+    OutputStream os;
+
+    public Producer(OutputStream o)
+    {
+        os=o;
+    }
+
+    public void run()
+    {
+        int count=1;
+
+        while(true)
+        {
+            try{
+                os.write(count);
+                os.flush();
+
+                System.out.println("Producer "+count);
+                System.out.flush();
+
+                Thread.sleep(10);
+                count++;
+            }catch(Exception e){}
+        }
+    }
+
+}
+
+class Consumer extends Thread
+{
+    InputStream is;
+
+    public Consumer(InputStream s)
+    {
+        is=s;
+    }
+
+    public void run()
+    {
+        int x;
+
+        while(true)
+        {
+            try{
+
+                x=is.read();
+
+                System.out.println("Consumer "+x);
+                System.out.flush();
+                Thread.sleep(10);
+
+            }catch(Exception e){}
+        }
+    }
+
+}
+
+
+
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
+        PipedInputStream pis=new PipedInputStream();
+        PipedOutputStream pos=new PipedOutputStream();
+
+        pos.connect(pis);
+
+        Producer p=new Producer(pos);
+        Consumer c=new Consumer(pis);
+
+        p.start();
+
+        c.start();
+
     }
 }
