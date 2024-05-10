ba0e379c4018968b5f2cf7ccda376a6b83755021
Ayush gopal <ayushgopal17@gmail.com>
2024-05-10 20:27:53 +0530

    serilisation and deserilisation

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,68 @@
 import java.io.*;
-import java.util.Date;
 
-class Student
+class Student implements Serializable
 {
-    int rollno;
-    String name;
-    String Dept;
+    private int rollno;
+    private String name;
+    private float avg;
+    private String dept;
+    public static int Data=10;
+    public transient int t;
+
+    public Student()
+    {
+
+    }
+    public Student(int r,String n,float a,String d)
+    {
+        rollno=r;
+        name=n;
+        avg=a;
+        dept=d;
+        Data=500;
+        t=500;
+    }
+
+    public String toString()
+    {
+        return "\nStudent Details\n"+
+                "\nRoll "+rollno+
+                "\nName "+name+
+                "\nAverage "+avg+
+                "\nDept "+dept+
+                "\nData "+Data+
+                "\nTransient "+t+"\n";
+    }
 
 }
-public class Main {
-    public static void main(String[] args) throws Exception {
-FileOutputStream fos=new FileOutputStream("C:\\myjava\\student.txt");
-        DataOutputStream dos=new DataOutputStream(fos);
-        Student s=new Student();
-        s.rollno=10;;
-        s.name="John";
-        s.Dept="CSE";
-        dos.writeInt(s.rollno);
-        dos.writeUTF(s.name);
-        dos.writeUTF(s.Dept);
 
-        dos.close();
-        fos.close();
+public class Main
+{
+    /* public static void main(String[] args) throws Exception
+     {
+         FileOutputStream fos=new FileOutputStream("C:\\MyJava\\Student3.txt");
+         ObjectOutputStream oos=new ObjectOutputStream(fos);
+
+         Student s=new Student(10,"John",89.9f,"CSE");
+
+         oos.writeObject(s);
+
+         fos.close();
+         oos.close();
+
+     }
+     */
+    public static void main(String[] args) throws Exception
+    {
+        FileInputStream fis=new FileInputStream("C:\\MyJava\\Student3.txt");
+        ObjectInputStream ois=new ObjectInputStream(fis);
+
+        Student s=(Student)ois.readObject();
+
+        System.out.println(s);
+
+        fis.close();
+        ois.close();
 
     }
 }
