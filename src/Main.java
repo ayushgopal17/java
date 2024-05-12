cd537968b305306ffb13ad73b24f6600611ca473
Ayush gopal <ayushgopal17@gmail.com>
2024-05-12 11:32:49 +0530

    Generic methods

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,37 @@
+import java.io.UncheckedIOException;
 
+interface A{}
 
-public class Main<T>
+class B implements A{}
+    class C implements A{}
+
+@SuppressWarnings("Unchecked")
+
+class MyArray<T>
+
 {
-    T data[]=(T[]) new Object[3];
-    public static void main(String[] args)
-    { Main<String> gd = new Main();
-        gd.data[0]="hi";
-        gd.data[1]="bye";
-        gd.data[2]=new Integer(10);
-        String str=gd.data[0];
+    T A[]=(T[]) new Object[10];
+    int length=0;
+    public void append(T v)
+    {
+        A[length++]=v;
     }
+    public void display()
+    {
+        for (int i=0;i<length;i++)
+        {
+            System.out.println(A[i]);
+        }
+    }
+}
+public class Main {
+    static <E> void show(E[] list)
+    {
+for (E x:list)
+    System.out.println(x);
+    }
+    public static void main(String[] args) {
 
+        show(new String[]{"hi","go","bye"});
+    }
 }
