d2e96531c1bb98516351a0ad10986427f69576b2
Ayush gopal <ayushgopal17@gmail.com>
2024-05-14 15:34:18 +0530

    linkedlist

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args) {
-        ArrayList<Integer> al1 = new ArrayList<>(20);
-        ArrayList<Integer> al12 = new ArrayList<>(List.of(40, 50, 60, 70, 80));
+        LinkedList<Integer> al1 = new LinkedList<>();
+        LinkedList<Integer> al12 = new LinkedList<>(List.of(40, 50, 60, 70, 80));
 
         al1.add(10);
         al1.add(0, 5);
@@ -14,7 +14,7 @@ public class Main{
     }
 static void show (int n)
 {
-    if (n > 60)
+
         System.out.println(n);
 
 }
