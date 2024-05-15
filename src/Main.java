37e0eb3629da39ddfa7e384d0cdef6f8b8ba9893
Ayush gopal <ayushgopal17@gmail.com>
2024-05-15 11:56:21 +0530

    priority queue reversing the order

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
 
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args) {
-       PriorityQueue<Integer> P=new PriorityQueue<>();
+class MyCom implements Comparator<Integer>
+{
+
+    public int compare(Integer o1, Integer o2)
+    {
+     if (o1<o2) return 1;
+     if (o1>o2) return -1;
+     return 0;
+    }
+}
+public class Main
+{
+    public static void main(String[] args)
+    {
+       PriorityQueue<Integer> P=new PriorityQueue<>(new MyCom());
        P.add(20);
        P.add(10);
        P.add(30);
