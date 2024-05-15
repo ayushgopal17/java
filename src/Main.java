48f4944efdcf838b08720b3edc742b00f72c3e0b
Ayush gopal <ayushgopal17@gmail.com>
2024-05-15 11:39:02 +0530

    priority Queue

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,16 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args) {
-        ArrayDeque<Integer> dq=new ArrayDeque<>();
-        dq.offerLast(10);
-        dq.offerLast(20);
-        dq.offerLast(30);
-        dq.offerLast(40);
+       PriorityQueue<Integer> P=new PriorityQueue<>();
+       P.add(20);
+       P.add(10);
+       P.add(30);
+       P.add(5);
+       P.add(15);
+       P.add(3);
 
 
-        dq.offerFirst(1);
-        dq.offerFirst(2);
-        dq.offerFirst(3);
-        dq.offerFirst(4);
-        dq.forEach((x)->System.out.println(x));
+        System.out.println(P.peek());
     }
 
 
