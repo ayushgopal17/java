c1bca99c909e2c808a6f278fb9544453d01efa96
Ayush gopal <ayushgopal17@gmail.com>
2024-05-15 11:21:45 +0530

    DEQUE

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,19 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args) {
-        LinkedList<Integer> al1 = new LinkedList<>();
-        LinkedList<Integer> al12 = new LinkedList<>(List.of(40, 50, 60, 70, 80));
+        ArrayDeque<Integer> dq=new ArrayDeque<>();
+        dq.offerLast(10);
+        dq.offerLast(20);
+        dq.offerLast(30);
+        dq.offerLast(40);
 
-        al1.add(10);
-        al1.add(0, 5);
-        al1.addAll(1, al12);
-        al1.set(6, 200);
-        al1.forEach(n->show(n));
+
+        dq.offerFirst(1);
+        dq.offerFirst(2);
+        dq.offerFirst(3);
+        dq.offerFirst(4);
+        dq.forEach((x)->System.out.println(x));
     }
-static void show (int n)
-{
 
-        System.out.println(n);
-
-}
 
     }
