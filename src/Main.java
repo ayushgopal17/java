6f2d3c3bbe895b77e9763f57bd2dff516acd2e73
Ayush gopal <ayushgopal17@gmail.com>
2024-05-15 12:30:30 +0530

    hashset

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,20 @@
 
 import java.util.*;
 
-class MyCom implements Comparator<Integer>
-{
-
-    public int compare(Integer o1, Integer o2)
-    {
-     if (o1<o2) return 1;
-     if (o1>o2) return -1;
-     return 0;
-    }
-}
 public class Main
 {
     public static void main(String[] args)
     {
-       PriorityQueue<Integer> P=new PriorityQueue<>(new MyCom());
-       P.add(20);
-       P.add(10);
-       P.add(30);
-       P.add(5);
-       P.add(15);
-       P.add(3);
+       HashSet<Integer> hs=new HashSet<>();
+
+       hs.add(10);
+       hs.add(20);
+       hs.add(30);
+       hs.add(10);
 
 
-        System.out.println(P.peek());
-        P.forEach((x) -> System.out.println(x));
 
-        P.poll();
-        System.out.println("After Deletion");
-        P.forEach((x) -> System.out.println(x));
+        System.out.println(hs);
+
     }
     }
