86345a82977bc34e0f0830edb51a066794595ee1
Ayush gopal <ayushgopal17@gmail.com>
2024-05-20 23:48:33 +0530

    Treemap and put

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ public class Main {
 
     public static void main(String[] args) {
         TreeMap<Integer,String> tm=new TreeMap<>(Map.of(0,"A",1,"B",2,"C",3,"D"));
+
+
+        tm.put(4,"E");
+        tm.put(5,"F");
         System.out.println(tm);
     }
 }
