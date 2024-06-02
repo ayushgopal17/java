7f3ca0e1fec71725bcd2c12d7ba6af1c2f60acd0
Ayush gopal <ayushgopal17@gmail.com>
2024-06-02 21:34:06 +0530

    comparator

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        BitSet bs1=new BitSet();
-
-        bs1.set(0);
-        bs1.set(2);
-        bs1.set(4);
-        bs1.set(6);
-        bs1.set(8);
-        BitSet bs2=new BitSet();
-
-      //  bs2.set(0);
-        bs2.set(1);
-        //      bs2.set(2);
-        bs2.set(3);
-        //  bs2.set(4);
-        bs2.set(5);
-        //   bs2.set(6);
-        bs2.set(7);
-        //   bs2.set(8);
-        bs1.flip(0, bs1.length());
-        System.out.println(bs1);
+  int a[]={2,4,6,8,1,3,5};
+        int b[]={2,4,6,8,1,3,5};
+        System.out.println(Arrays.compare(a, b));
 
     }
 }
