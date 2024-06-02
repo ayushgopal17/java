12672cf0f5e9f55cd5689d8e6aec76a905a7e036
Ayush gopal <ayushgopal17@gmail.com>
2024-06-02 20:14:58 +0530

    bitset flip

--- src/Main.java
+++ src/Main.java
@@ -15,16 +15,16 @@ public class Main {
         bs1.set(8);
         BitSet bs2=new BitSet();
 
-        bs2.set(0);
+      //  bs2.set(0);
         bs2.set(1);
-        bs2.set(2);
+        //      bs2.set(2);
         bs2.set(3);
-        bs2.set(4);
+        //  bs2.set(4);
         bs2.set(5);
-        bs2.set(6);
+        //   bs2.set(6);
         bs2.set(7);
-        bs2.set(8);
-        bs1.and(bs2);
+        //   bs2.set(8);
+        bs1.flip(0, bs1.length());
         System.out.println(bs1);
 
     }
