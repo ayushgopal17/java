7be1d941f59e26d6f825b820adce658a3f00f77c
Ayush gopal <ayushgopal17@gmail.com>
2024-06-02 19:14:37 +0530

    String Tokenizer

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,17 @@ import java.util.*;
 import java.io.*;
 public class Main {
 
-    public static void main(String[] args)  {
-        String data="name=vijay;adress=delhi;country=india,dept=cse";
+    public static void main(String[] args) {
+        String data = "name=vijay adress=delhi country=india dept=cse";
 
-        StringTokenizer stk = new StringTokenizer(data,"=;");
-        //
+        StringTokenizer stk = new StringTokenizer(data, "= ");
+        String s;
+
+        while (stk.hasMoreTokens())
+        {
+            s=stk.nextToken();
+            System.out.println(s);
+        }
 
        }
 }
