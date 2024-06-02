94b5067451a791680eab2b6126525353a8c7522f
Ayush gopal <ayushgopal17@gmail.com>
2024-06-02 21:41:22 +0530

    Array and fill

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,12 @@ public class Main {
     public static void main(String[] args) {
 
   int a[]={2,4,6,8,1,3,5};
-        int b[]={2,4,6,8,1,3,5};
-        System.out.println(Arrays.compare(a, b));
+  int b[]={2,4,6,8,1,3,5};
+
+  int c[]=Arrays.copyOf(a,a.length);
+Arrays.fill(c,10);
+for (int x:c)
+        System.out.println(x);
 
     }
 }
