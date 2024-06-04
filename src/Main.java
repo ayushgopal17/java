ac84b37d9e2b957e60ceed8ba053d0747d9b927d
Ayush gopal <ayushgopal17@gmail.com>
2024-06-04 23:25:49 +0530

    multithreaded Reverse echo

--- src/Main.java
+++ src/Main.java
@@ -1,78 +1,45 @@
 import java.net.*;
 import java.io.*;
 
-public class Main extends Thread
+public class Main implements Runnable
 {
-    Socket stk;
-
-    public Main(Socket st)
+    Socket client;
+    public Main(Socket stk)
     {
-        stk=st;
+        client=stk;
     }
-    public void run()
-    {
-        try{
-            BufferedReader br=new BufferedReader(new InputStreamReader(stk.getInputStream()));
-            PrintStream ps=new PrintStream(stk.getOutputStream());
 
-            String msg;
-            StringBuilder sb;
-            do
-            {
-                msg=br.readLine();
-
-                sb=new StringBuilder(msg);
-                sb.reverse();
-                msg=sb.toString();
-
-                ps.println(msg);
-
-            }while(!msg.equals("dne"));
-
-            stk.close();
-        }catch(Exception e){}
-
-    }
 
     public static void main(String[] args) throws Exception
     {
         ServerSocket ss=new ServerSocket(2000);
-        int count=1;
+
         Socket stk;
-        Main re;
         do
         {
             stk=ss.accept();
-            System.out.println("Client Connected :"+count++);
-            re=new Main(stk);
-            re.start();
-
+            System.out.println("Client Connected");
+            Main mt=new Main(stk);
+            Thread t=new Thread(mt);
+            t.start();
         }while(true);
 
     }
-}
-
-
-class Client
-{
-    public static void main(String[] args) throws Exception
+    public void run()
     {
-        Socket stk=new Socket("192.168.0.103",2000);
+        try{
 
-        BufferedReader keyb=new BufferedReader(new InputStreamReader(System.in));
-        BufferedReader br=new BufferedReader(new InputStreamReader(stk.getInputStream()));
-        PrintStream ps=new PrintStream(stk.getOutputStream());
+            StringBuffer buff;
+            BufferedReader br=new BufferedReader(new InputStreamReader(client.getInputStream()));
+            PrintStream ps=new PrintStream(client.getOutputStream());
+            String str;
+            do
+            {
+                str=br.readLine();
+                buff=new StringBuffer(str);
+                ps.println(buff.reverse());
+            }while(!str.equals("bye"));
 
-        String msg;
-
-        do
-        {
-            msg=keyb.readLine();
-            ps.println(msg);
-            msg=br.readLine();
-            System.out.println("From Server"+msg);
-
-        }while(!msg.equals("dne"));
-        stk.close();
+        }catch(Exception e){}
     }
 }
