591d9242664455e3947460cdcb93351e80392b78
Ayush gopal <ayushgopal17@gmail.com>
2024-06-04 20:36:19 +0530

    Reverse echo

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,78 @@
-import java.time.format.DateTimeFormatter;
-import java.util.*;
-import java.time.*;
-import java.time.temporal.*;
+import java.net.*;
+import java.io.*;
+
+public class Main extends Thread
+{
+    Socket stk;
+
+    public Main(Socket st)
+    {
+        stk=st;
+    }
+    public void run()
+    {
+        try{
+            BufferedReader br=new BufferedReader(new InputStreamReader(stk.getInputStream()));
+            PrintStream ps=new PrintStream(stk.getOutputStream());
+
+            String msg;
+            StringBuilder sb;
+            do
+            {
+                msg=br.readLine();
+
+                sb=new StringBuilder(msg);
+                sb.reverse();
+                msg=sb.toString();
+
+                ps.println(msg);
+
+            }while(!msg.equals("dne"));
+
+            stk.close();
+        }catch(Exception e){}
+
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        ServerSocket ss=new ServerSocket(2000);
+        int count=1;
+        Socket stk;
+        Main re;
+        do
+        {
+            stk=ss.accept();
+            System.out.println("Client Connected :"+count++);
+            re=new Main(stk);
+            re.start();
+
+        }while(true);
+
+    }
+}
 
 
+class Client
+{
+    public static void main(String[] args) throws Exception
+    {
+        Socket stk=new Socket("192.168.0.103",2000);
 
-class Main {
+        BufferedReader keyb=new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br=new BufferedReader(new InputStreamReader(stk.getInputStream()));
+        PrintStream ps=new PrintStream(stk.getOutputStream());
 
+        String msg;
 
-    public static void main(String[] args) {
-LocalDateTime dt = LocalDateTime.now();
+        do
+        {
+            msg=keyb.readLine();
+            ps.println(msg);
+            msg=br.readLine();
+            System.out.println("From Server"+msg);
 
-        System.out.println(dt.get(ChronoField.AMPM_OF_DAY));
+        }while(!msg.equals("dne"));
+        stk.close();
     }
 }
